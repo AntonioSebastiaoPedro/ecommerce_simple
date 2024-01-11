a28885388211448ea44593b43b874d3ff43cedfc
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2024-01-11 23:48:27 +0100

    chore: Configura o directório dos arquivos

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 #arquivos directórios raízes
-$PastaInterna = "ecommerce_simple/";
+$PastaInterna = "";
 define("DIRPAGE", "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
 if (substr("{$_SERVER['DOCUMENT_ROOT']}", -1) === '/') {
 	define("DIRREQ", "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
